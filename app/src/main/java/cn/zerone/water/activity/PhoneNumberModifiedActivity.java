1c2a229be7d2b7b375f1ebe9833a7bf7fbf943f2
qhk_yang <qinghuanke@163.com>
2019-05-08 07:52:50 +0800

    添加手机短信验证码jar包，待解决

--- app/src/main/java/cn/zerone/water/activity/PhoneNumberModifiedActivity.java
+++ app/src/main/java/cn/zerone/water/activity/PhoneNumberModifiedActivity.java
@@ -3,15 +3,27 @@ package cn.zerone.water.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import cn.zerone.water.R;
 
 public class PhoneNumberModifiedActivity extends AppCompatActivity {
+    String APPKEY = "";
+    String APPSERECT = "";
+
+    private EditText inputPhone;
+
+    private Button verificationCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_number_modify);
+        verificationCode = (Button) findViewById(R.id.login_verificate_code);
+
+//        MobSDK.init(this, APPKEY, APPSERECT);
 
     }
 }
