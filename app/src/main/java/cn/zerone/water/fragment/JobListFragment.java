1b94870fdfd2357a51086bb8167ce1da4f1b9ef2
Lee <leegovjane@163.com>
2019-05-08 22:29:42 +0800

    add task

--- app/src/main/java/cn/zerone/water/fragment/JobListFragment.java
+++ app/src/main/java/cn/zerone/water/fragment/JobListFragment.java
@@ -1,18 +1,56 @@
 package cn.zerone.water.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.view.View.OnClickListener;
+
+import cn.zerone.water.R;
+import cn.zerone.water.activity.MealActivity;
 
 /**
  * Created by zero on 2018/12/3.
  */
 
-public class JobListFragment extends WebFragment {
+public class JobListFragment extends Fragment {
+
+    private ListView task_listView;
+    private  Button but_nvi;
     @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_job,null);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        task_listView = view.findViewById(R.id.task_list_view);
+        but_nvi = view.findViewById(R.id.button_nvi);
+        but_nvi.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MealActivity.class);
+                getContext().startActivity(intent);
+            }
+    });
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+
+
+
+
     public String title() {
         return  "任务";
     }
