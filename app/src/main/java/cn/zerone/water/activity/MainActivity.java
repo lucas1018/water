7ae98b79c9944be7810521023b03812aec3ec661
yangzheng <1135144141@qq.com>
2019-05-28 18:25:36 +0800

    修改首页导航栏

--- app/src/main/java/cn/zerone/water/activity/MainActivity.java
+++ app/src/main/java/cn/zerone/water/activity/MainActivity.java
@@ -20,6 +20,7 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.alibaba.fastjson.JSONObject;
@@ -45,6 +46,7 @@ import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
     public static MainActivity activity;
+    private ImageView home;
 
     List<String> mViewList = new ArrayList<String>();//顶部用于循环的布局集合
     Fragment[] fragments = new Fragment[]{new NoticeFragment(),new JobListFragment(),new HomeFragment(),new LiveFragment(),new MyselfFragment()};
@@ -58,6 +60,12 @@ public class MainActivity extends AppCompatActivity {
             }else{
                 transaction.hide(fragments[i]);
             }
+        }
+        if (index == 2 ){
+            home.setImageResource(R.drawable.home_blue);
+        }
+        else{
+            home.setImageResource(R.drawable.home_gray);
         }
         transaction.commit();
 
@@ -130,6 +138,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        home = (ImageView)findViewById(R.id.navigation_center_image);
 
         ((App)getApplication()).mapInit();
 
@@ -150,6 +159,7 @@ public class MainActivity extends AppCompatActivity {
         int b=navigation.getMaxItemCount();
         BottomNavigationMenuView menuView  = (BottomNavigationMenuView) navigation.getChildAt(0);
         navigation.setSelectedItemId(navigation.getMenu().getItem(2).getItemId());
+        home.setImageResource(R.drawable.home_blue);
         View tab = menuView.getChildAt(1);
         BottomNavigationItemView itemView = (BottomNavigationItemView) tab;
          badge = LayoutInflater.from(this).inflate(R.layout.menu_badge, null, false);
