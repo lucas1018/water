1ab4e6d160cff2f23f02e22cfff28a4daa359b07
yangzheng <1135144141@qq.com>
2019-05-30 17:00:03 +0800

    添加device——tokens

--- app/src/main/java/cn/zerone/water/activity/LoginActivity.java
+++ app/src/main/java/cn/zerone/water/activity/LoginActivity.java
@@ -6,37 +6,22 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.PermissionChecker;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
-
 
 import java.util.ArrayList;
 import java.util.List;
@@ -203,7 +188,7 @@ public class LoginActivity extends AppCompatActivity  {
                 @Override
                 public void onComplete() {
                 }
-            },email,password);
+            },email,password,App.device_tokens);
         }
     }
 
