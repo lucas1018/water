893aee05b596b96b0531449c9edacb45515ef632
Pierre Von <kingpingye@yahoo.com>
2019-05-30 14:13:57 +0800

    base64“

--- app/src/main/java/cn/zerone/water/utils/imageToBase64.java
+++ app/src/main/java/cn/zerone/water/utils/imageToBase64.java
@@ -3,10 +3,11 @@ package cn.zerone.water.utils;
 import android.text.TextUtils;
 import android.util.Base64;
 
+import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import sun.misc.BASE64Encoder;
 
 public class imageToBase64 {
     private String path;
@@ -20,31 +21,44 @@ public class imageToBase64 {
     }
 
     public String getBase64() {
-        if (TextUtils.isEmpty(getPath())) {
-            return null;
-        }
-        InputStream is = null;
-        byte[] data;
-        String result = null;
+//        if (TextUtils.isEmpty(getPath())) {
+//            return null;
+//        }
+//        InputStream is = null;
+//        byte[] data;
+//        String result = null;
+//        try {
+//            is = new FileInputStream(getPath());
+//            //创建一个字符流大小的数组。
+//            data = new byte[is.available()];
+//            //写入数组
+//            is.read(data);
+//            //用默认的编码格式进行编码
+//            result = Base64.encodeToString(data, Base64.DEFAULT);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (null != is) {
+//                try {
+//                    is.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//        return result;
+        File file = new File(path);
+        String imageStr="";
         try {
-            is = new FileInputStream(getPath());
-            //创建一个字符流大小的数组。
-            data = new byte[is.available()];
-            //写入数组
-            is.read(data);
-            //用默认的编码格式进行编码
-            result = Base64.encodeToString(data, Base64.DEFAULT);
+            InputStream in = new BufferedInputStream(new FileInputStream(file));
+            byte[] data = new byte[in.available()];
+            in.read(data);
+            in.close();
+            imageStr = new String(Base64.encodeToString(data, Base64.DEFAULT));
         } catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
-        } finally {
-            if (null != is) {
-                try {
-                    is.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
-        return result;
+        return imageStr;
     }
 }
