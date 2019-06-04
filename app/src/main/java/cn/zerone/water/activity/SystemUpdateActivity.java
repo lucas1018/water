79171d3daf50c202ef6ebaad76058edbfd0c57c7
yaoliangyong <yaoliangyong221@163.com>
2019-06-04 10:19:35 +0800

    chenjia add systemUpdate

--- app/src/main/java/cn/zerone/water/activity/SystemUpdateActivity.java
+++ app/src/main/java/cn/zerone/water/activity/SystemUpdateActivity.java
@@ -1,5 +1,6 @@
 package cn.zerone.water.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -10,14 +11,22 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.baidu.baidumaps.common.Constant;
+
 import org.w3c.dom.Text;
 
 import cn.zerone.water.R;
+import cn.zerone.water.service.UpdateService;
+import cn.zerone.water.views.UpdataDialog;
 
 public class SystemUpdateActivity extends AppCompatActivity {
 
     private String updateUrl;
-
+    private UpdataDialog updataDialog;
+    private int  isAutoUpdate=0;
+    private TextView tvmsg;
+    //todo 这个参数要提取全局变量 要不然当activity杀死之后 其他地方存在他的引用，容易造成内存泄漏
+    public static String APK_DOWNLOAD_URL="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,24 +50,48 @@ public class SystemUpdateActivity extends AppCompatActivity {
                 btn_refresh.setVisibility(View.INVISIBLE);
             } else {
                 btn_refresh.setVisibility(View.VISIBLE);
-//                btn_refresh.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        System.out.println("pppppppppppp");
-//                        /**调用系统自带的浏览器去下载最新apk*/
-//                        updateUrl = "http://47.33333333";
-//                        Intent intent = new Intent();
-//                        intent.setAction("android.intent.action.VIEW");
-//                        if (updateUrl != null && updateUrl.startsWith("http")) {
-//                            Uri content_url = Uri.parse(updateUrl);
-//                            intent.setData(content_url);
-//                            startActivity(intent);
-//                        }
-//                    }
-//                });
+                //todo isAutoUpdate   updateUrl  都是从接口获取的 这要发起一个数据请求
+                btn_refresh.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        updataDialog = new UpdataDialog(SystemUpdateActivity.this, R.layout.dialog_updataversion,
+                                new int[]{R.id.dialog_sure, R.id.relative_imagv_dialog_cancel}, isAutoUpdate);
+                        updataDialog.show();
+                        tvmsg = (TextView) updataDialog.findViewById(R.id.updataversion_msg);
+                        tvmsg.setText("更改了bug");
+                        updataDialog.setOnCenterItemClickListener(new UpdataDialog.OnCenterItemClickListener() {
+                            @Override
+                            public void OnCenterItemClick(UpdataDialog dialog, View view) {
+                                switch (view.getId()) {
+                                    case R.id.dialog_sure:
+
+//                            /**调用系统自带的浏览器去下载最新apk*/
+//                            Intent intent = new Intent();
+//                            intent.setAction("android.intent.action.VIEW");
+//                            if (updateUrl != null && updateUrl.startsWith("http")) {
+//                                Uri content_url = Uri.parse(updateUrl);
+//                                intent.setData(content_url);
+//                                startActivity(intent);
+//                            }
+                                        goToDownload(SystemUpdateActivity.this, updateUrl);
+                                        break;
+                                }
+
+                            }
+                        });
+                    }
+                });
+
             }
         }catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    private void goToDownload(Context context, String updateUrl) {
+        Intent intent = new Intent();
+        intent.setClass(context, UpdateService.class);
+        intent.putExtra(APK_DOWNLOAD_URL, updateUrl);
+        context.startService(intent);
     }
 }
